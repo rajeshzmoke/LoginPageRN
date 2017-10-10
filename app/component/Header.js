64bbca01d3f90f5a40f45a420350c2681441e96e
rajeshzmoke <swapnesh@razorthinks.local>
2017-10-10 11:43:20 +0530

    first

---
+++ app/component/Header.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import {
+  View,
+  StyleSheet,
+  Text,
+  Image,
+} from 'react-native';
+
+export default class ProfilePage extends Component{
+
+  render(){
+    return(
+        <Image style={styles.headerBackground} source={require('../img/image1.jpg')}>
+          <View style={styles.header}>
+            <Image
+          </View>
+        </Image>
+
+
+    )
+  }
+}
